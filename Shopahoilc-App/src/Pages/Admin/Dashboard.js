8f6671d7c32ef2d05b6ff533cb615ebbffcf5254
Yogesh Yadav <yy072156@gmail.com>
2022-11-09 21:00:45 +0530

    admin/dashbord-working

--- Shopahoilc-App/src/Pages/Admin/Dashboard.js
+++ Shopahoilc-App/src/Pages/Admin/Dashboard.js
@@ -1,11 +1,121 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    FormControl,
+    FormLabel,
+    Input,
+    useDisclosure,
+    
+  } from '@chakra-ui/react'
+import {
+    Box,
+    Image,
+    Text,
+    SimpleGrid,
+    Button,
+    
+  } from '@chakra-ui/react';
 
+ 
 function Dashboard() {
-  return (
-    <div>
-      Dashboard
-    </div>
-  )
+
+    const { isOpen, onOpen, onClose } = useDisclosure()
+  
+    const initialRef = React.useRef(null)
+    const finalRef = React.useRef(null)
+  
+    const [data, setProd] = useState([])
+    useEffect(() => {
+        axios.get("http://localhost:8080/data").then((d) => {
+          setProd(d.data);
+        });
+      }, []);
+    //   console.log(data)
+
+      return (
+<Box >
+<SimpleGrid   columns={4}  spacingX='40px' spacingY='50px'>
+
+{data.map((item) => (
+    <Box  margin="auto" padding={8} border='1px' borderColor='lightblue'>
+     <div key={item.id}>
+        <Image
+          src={item.imageUrl}
+          alt={`Picture of ${item.name}`}
+          roundedTop="lg"
+        />
+  <Text as='b'   color="black"> {item.name} </Text> <br />
+  <Text as="b" color="grey">Category: {item.category}</Text>
+  <Text  color="grey" >Brand: {item.brand}</Text>
+
+   <Text color="grey"> Stars:{item.stars}</Text> 
+  
+  <Text  color="grey" >Price: {item.price}</Text>
+
+  <Button  marginTop={5} colorScheme='red' variant='outline'>Delete</Button>
+  
+   <Button onClick={onOpen}  marginTop={5}   marginLeft="5" colorScheme='blue' variant='outline'>Update</Button>
+  
+        </div>
+        </Box>
+
+      ))}
+       
+        <Modal
+          initialFocusRef={initialRef}
+          finalFocusRef={finalRef}
+          isOpen={isOpen}
+          onClose={onClose}
+        >
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Create your account</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6}>
+              <FormControl>
+                <FormLabel> name</FormLabel>
+                <Input ref={initialRef} placeholder='Product Name' />
+              </FormControl>
+  
+              <FormControl mt={4}>
+                <FormLabel>Category</FormLabel>
+                <Input placeholder='like: Mackup,hair..' />
+              </FormControl>
+              
+              <FormControl mt={4}>
+                <FormLabel>Brand</FormLabel>
+                <Input placeholder='Brand Name ' />
+              </FormControl>
+
+              <FormControl mt={4}>
+                <FormLabel>Price</FormLabel>
+                <Input placeholder='In ₹ ' />
+              </FormControl>
+            </ModalBody>
+  
+            <ModalFooter>
+              <Button colorScheme='blue' mr={3}>
+                Save
+              </Button>
+              <Button onClick={onClose}>Cancel</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+
+
+</SimpleGrid>
+</Box>
+
+
+
+      );
 }
 
 export default Dashboard
