b3ec4e259966a6aea5be693c43882a9f98b87537
Akash kumar Panda <107460709+AkashPanda1701@users.noreply.github.com>
2022-11-10 22:33:58 +0530

    Update App.tsx

--- Shopahoilc-App/src/App.tsx
+++ Shopahoilc-App/src/App.tsx
@@ -3,7 +3,6 @@ import './App.css';
 import AllRoutes from './All-Routes/AllRoutes';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
-import SingleProduct from './Pages/Product/SingleProduct';
 
 /*
 Dont make any changes to this file
