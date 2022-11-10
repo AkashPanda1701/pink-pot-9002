29e1afc727841108bce16a7a496f400c22cf5039
Nikhil-81 <nikhilwalwatkar81@gmail.com>
2022-11-10 15:39:59 +0530

    css added to Products

--- Shopahoilc-App/src/App.tsx
+++ Shopahoilc-App/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import AllRoutes from './All-Routes/AllRoutes';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
+import SingleProduct from './Pages/Product/SingleProduct';
 
 /*
 Dont make any changes to this file
