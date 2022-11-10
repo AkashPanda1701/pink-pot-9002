ccb23ddf88261d3e37bcd09faf43dbb072d61284
piyush-agrawal6 <agrawaljoy1@gmail.com>
2022-11-10 10:13:57 +0530

    navbar fixed

--- Shopahoilc-App/src/Pages/Home/Home.js
+++ Shopahoilc-App/src/Pages/Home/Home.js
@@ -1,12 +1,8 @@
 import React from "react";
-import Footer from "../../Components/Footer/Footer";
-import Navbar from "../../Components/Navbar/Navbar";
 function Home() {
   return (
     <div>
-      <Navbar />
       Home
-      <Footer />
     </div>
   );
 }
