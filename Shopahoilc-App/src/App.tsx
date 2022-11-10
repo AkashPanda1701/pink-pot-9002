0808d6c7e561235c71a74b4ee80d6d5b7684977a
Akash kumar Panda <107460709+AkashPanda1701@users.noreply.github.com>
2022-11-10 09:42:13 +0530

    Update App.tsx

--- Shopahoilc-App/src/App.tsx
+++ Shopahoilc-App/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import AllRoutes from './All-Routes/AllRoutes';
+import Navbar from '../Components/Navbar/Navbar';
+import Footer from '../Components/Footer/Footer';
 
 /*
 Dont make any changes to this file
@@ -9,7 +11,9 @@ Dont make any changes to this file
 function App() {
   return (
     <>
+      <Navbar/>
       <AllRoutes />
+      <Footer/>
     </>
   );
 }
